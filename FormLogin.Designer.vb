17acec8a41188b5e8ed59bd7629ddabd2ae1cdb8
Violet Adhiambo <adhiamboviolet9@gmail.com>
2021-05-28 16:12:50 +0300

    all files added

---
+++ FormLogin.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormLogin))
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.labelPassword = New System.Windows.Forms.Label()
+        Me.labelDesignation = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.labelUsername = New System.Windows.Forms.Label()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(94, 55)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(253, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'labelPassword
+        '
+        Me.labelPassword.AutoSize = True
+        Me.labelPassword.Location = New System.Drawing.Point(14, 58)
+        Me.labelPassword.Name = "labelPassword"
+        Me.labelPassword.Size = New System.Drawing.Size(53, 13)
+        Me.labelPassword.TabIndex = 3
+        Me.labelPassword.Text = "Password"
+        '
+        'labelDesignation
+        '
+        Me.labelDesignation.AutoSize = True
+        Me.labelDesignation.Location = New System.Drawing.Point(14, 93)
+        Me.labelDesignation.Name = "labelDesignation"
+        Me.labelDesignation.Size = New System.Drawing.Size(63, 13)
+        Me.labelDesignation.TabIndex = 5
+        Me.labelDesignation.Text = "Designation"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(94, 20)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(253, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Student", "Lecturer", "Administrator"})
+        Me.ComboBox1.Location = New System.Drawing.Point(94, 90)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(253, 21)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(205, 136)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(142, 33)
+        Me.btnLogin.TabIndex = 0
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(17, 136)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(142, 33)
+        Me.btnCancel.TabIndex = 7
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'labelUsername
+        '
+        Me.labelUsername.AutoSize = True
+        Me.labelUsername.Location = New System.Drawing.Point(14, 23)
+        Me.labelUsername.Name = "labelUsername"
+        Me.labelUsername.Size = New System.Drawing.Size(55, 13)
+        Me.labelUsername.TabIndex = 1
+        Me.labelUsername.Text = "Username"
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 7)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnLogin)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.labelUsername)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.labelDesignation)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnCancel)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.TextBox1)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.labelPassword)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.TextBox2)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.ComboBox1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(650, 180)
+        Me.SplitContainer1.SplitterDistance = 288
+        Me.SplitContainer1.TabIndex = 8
+        '
+        'FormLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(651, 194)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FormLogin"
+        Me.Text = "Lecturer Evaluation System Login"
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents labelPassword As Label
+    Friend WithEvents labelDesignation As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents labelUsername As Label
+    Friend WithEvents SplitContainer1 As SplitContainer
+End Class
---
+++ FormLogin.vb
@@ -0,0 +1,6 @@
+﻿Public Class FormLogin
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        FormStudentFeedback.Show()
+    End Sub
+
+End Class
